b36f7eb51df30833c9a0c85783db2f821a609848
emmapreysing <emma@preysingmail.com>
2023-11-13 16:51:02 +0100

    Display Random 10 Letters

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,10 @@
+class GamesController < ApplicationController
+  def new
+    # Lookup table of lowercase letters.
+    @letters = []
+    random_letters = ('a'..'z').to_a.sample(10)
+    random_letters.each do |letter|
+      @letters << letter
+    end
+  end
+end
